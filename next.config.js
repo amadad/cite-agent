161dfdfbbf4b4faf935547d8e8af41e225a224bc
amadad <ali@scty.org>
2023-09-01 10:45:44 -0400

    Azure optimizations

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
-/** @type {import('next').NextConfig} */
+/** @type {import('next').NextConfig} 
 const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = nextConfig */
+
+
+module.exports = {
+  reactStrictMode: true,
+  output: "standalone"
+}
+
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,46 +3,34 @@ import { Search } from "@/components/Search";
 import { SearchQuery } from "@/types";
 import { IconBrandGithub, IconBrandTwitter } from "@tabler/icons-react";
 import Head from "next/head";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function Home() {
+  const [time, setTime] = useState<Date | null>(null);
   const [searchQuery, setSearchQuery] = useState<SearchQuery>({ query: "", sourceLinks: [] });
   const [answer, setAnswer] = useState<string>("");
   const [done, setDone] = useState<boolean>(false);
+
+  useEffect(() => {
+    fetch('/api/time')
+    .then(res => res.json())
+    .then(json => setTime(new Date(json.time)));
+  }, []);
 
   return (
     <>
       <Head>
         <title>SCTY — Cite</title>
-        <meta
-          name="description"
-          content="AI-powered search."
-        />
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1"
-        />
-        <link
-          rel="icon"
-          href="/favicon.png"
-        />
+        <meta name="description" content="AI-powered search." />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.png" />
       </Head>
       <div className="h-screen overflow-auto bg-[#18181C] text-[#D4D4D8]">
-        <a
-          className="absolute top-0 right-12 p-4 cursor-pointer"
-          href="https://twitter.com/sctyinc"
-          target="_blank"
-          rel="noreferrer"
-        >
+        <a className="absolute top-0 right-12 p-4 cursor-pointer" href="https://twitter.com/sctyinc" target="_blank" rel="noreferrer">
           <IconBrandTwitter />
         </a>
 
-        <a
-          className="absolute top-0 right-2 p-4 cursor-pointer"
-          href="https://github.com/amadad/"
-          target="_blank"
-          rel="noreferrer"
-        >
+        <a className="absolute top-0 right-2 p-4 cursor-pointer" href="https://github.com/amadad/" target="_blank" rel="noreferrer">
           <IconBrandGithub />
         </a>
 
@@ -63,6 +51,12 @@ export default function Home() {
             onAnswerUpdate={(value) => setAnswer((prev) => prev + value)}
             onDone={setDone}
           />
+        )}
+
+        {time && (
+          <div className="absolute bottom-4 left-4">
+            Current Time: {time.getHours()}:{time.getMinutes()}:{time.getSeconds()}
+          </div>
         )}
       </div>
     </>
